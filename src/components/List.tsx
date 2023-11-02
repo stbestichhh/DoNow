41b18c9fc8228a32bec9576396497146552246de
Kirill Yaymchuk <stbestich@gmail.com>
2023-11-02 23:25:11 +0200

    Added new todo form

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,13 @@
 import { Text, SafeAreaView, TouchableOpacity, View, FlatList } from 'react-native';
-import React, { useCallback, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { ListStyle } from '../styles';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Entypo from '@expo/vector-icons';
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import BottomSheet, { BottomSheetTextInput, BottomSheetView } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { FIREBASE_DB } from '../../firebaseConfig';
+import { Button } from '@excalidraw/excalidraw';
 
 export interface Todo {
 	title: string;
@@ -16,14 +17,14 @@ export interface Todo {
 
 const List = () => {
 	const sheetRef = useRef<BottomSheet>(null);
-	const snapPoints = useMemo(() => ['1%', '50%', '90%'], []);
+	const snapPoints = useMemo(() => ['1%', '25%', '50%', '90%'], []);
 
 	//callbacks
-	const handleSheetChange = useCallback((index: any) => {
+	const handleSheetChange = useCallback((index: number) => {
 		console.log('handleSheetChange', index);
 	}, []);
 
-	const handleSnapPress = useCallback((index: any) => {
+	const handleSnapPress = useCallback((index: number) => {
 		sheetRef.current?.snapToIndex(index);
 	}, []);
 
@@ -34,6 +35,32 @@ const List = () => {
 	//todos
 	const [todos, setTodos] = useState<Todo[]>([]);
 	const [todo, setTodo] = useState('');
+
+	useEffect(() => {
+		const ref = collection(FIREBASE_DB, 'todos');
+
+		const subscribe = onSnapshot(ref, {
+			next: (snapshot) => {
+				const todos: Todo[] = [];
+				snapshot.docs.forEach((doc) => {
+					todos.push({
+						id: doc.id,
+						...doc.data(),
+					} as Todo);
+				});
+				setTodos(todos);
+			},
+		});
+		return () => subscribe();
+	}, []);
+
+	const addTodoItem = async () => {
+		const doc = addDoc(collection(FIREBASE_DB, 'todos'), {
+			title: todo,
+			done: false,
+		});
+		setTodo('');
+	};
 
 	const renderTodo = ({ item }: any) => {
 		const ref = doc(FIREBASE_DB, `todos/${item.id}`);
@@ -50,10 +77,10 @@ const List = () => {
 			<View style={ListStyle.container}>
 				<TouchableOpacity onPress={toggleDone} style={ListStyle.todoItem}>
 					{item.done && <Ionicons name='md-checkmark-circle' color='green' size={17} />}
-          <Text style={ListStyle.todoText}>{item.title}</Text>
+					<Text style={ListStyle.todoText}>{item.title}</Text>
 				</TouchableOpacity>
 
-        <Ionicons name="close" size={24} color="red" />
+				<Ionicons name='close' size={24} color='red' onPress={deleteItem}/>
 			</View>
 		);
 	};
@@ -72,9 +99,17 @@ const List = () => {
 				</TouchableOpacity>
 				<FlatList data={todos} renderItem={renderTodo} keyExtractor={(todo: Todo) => todo.id} />
 			</SafeAreaView>
-			<BottomSheet ref={sheetRef} snapPoints={snapPoints} onChange={handleSheetChange}>
+			<BottomSheet
+				ref={sheetRef}
+				index={0}
+				snapPoints={snapPoints}
+				onChange={handleSheetChange}
+				keyboardBehavior='extend'
+			>
 				<BottomSheetView>
-					<Text>BottomSheet</Text>
+					<Text style={ListStyle.formText}>Todo name</Text>
+					<BottomSheetTextInput style={ListStyle.formInput} onChangeText={(text: string) => setTodo(text)} value={todo}/>
+					<Ionicons name='add-circle' size={50} color='#CD5C5C' style={ListStyle.formButton} onPress={addTodoItem}/>
 				</BottomSheetView>
 			</BottomSheet>
 		</GestureHandlerRootView>
--- src/styles.tsx
+++ src/styles.tsx
@@ -2,62 +2,86 @@ import { StyleSheet } from 'react-native';
 
 export const LoginStyle = StyleSheet.create({
 	container: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 300,              
-    },
-	title: {                
-        fontSize: 40,
-        fontWeight: 'bold',
-        color: '#32CD32',        
-    },
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginTop: 300,
+	},
+	title: {
+		fontSize: 40,
+		fontWeight: 'bold',
+		color: '#32CD32',
+	},
 	form: {
-        justifyContent: 'center',
-        margin: 20,    
-        marginTop: 30         
-    },
-    input: {
-        backgroundColor: 'white',     
-        borderRadius: 10,
-        margin: 5,
-        padding: 15
-    },
-    buttonsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',                
-    },    
-    btnText: {
-      color: 'white',
-      fontWeight: 'bold'
-    }
+		justifyContent: 'center',
+		margin: 20,
+		marginTop: 30,
+	},
+	input: {
+		backgroundColor: 'white',
+		borderRadius: 10,
+		margin: 5,
+		padding: 15,
+	},
+	buttonsContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	btnText: {
+		color: 'white',
+		fontWeight: 'bold',
+	},
 });
 
 export const ListStyle = StyleSheet.create({
-    container:{
-        flexDirection: 'row',
-        alignContent: 'center',
-        padding: 20,
-        marginVertical: 4,
-        borderRadius: 10,
-        backgroundColor: 'white'
-    },
-    titleContainer: {
-        flexDirection: 'row',
-        margin: 20,        
-    },
-    title: {        
-        fontSize: 30,
-        fontWeight: '800',        
-        color: '#89C34E'        
-    },    
-    todoItem: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    todoText: {
-        flex: 1,
-        paddingHorizontal: 10,
-        fontSize: 20
-    }
-})
+	container: {
+		flexDirection: 'row',
+		alignContent: 'center',
+		padding: 15,
+		marginVertical: 4,
+        marginHorizontal: 10,
+		borderRadius: 20,
+		backgroundColor: 'white',
+	},
+	titleContainer: {
+		flexDirection: 'row',
+		margin: 20,
+	},
+	title: {
+		fontSize: 30,
+		fontWeight: '800',
+		color: '#89C34E',
+	},
+	todoItem: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',        
+	},
+	todoText: {
+		flex: 1,
+		paddingHorizontal: 10,
+		fontSize: 20,
+        fontWeight: 'bold'        
+	},
+	formText: {
+		paddingHorizontal: 10,
+		margin: 10,
+		fontSize: 20,
+		fontWeight: 'bold',
+		color: '#89C34E',
+	},
+	formInput: {
+		height: 40,
+		borderWidth: 0.2,
+		borderRadius: 10,
+		padding: 10,
+		margin: 10,
+		color: '#CD5C5C',
+		fontWeight: 'bold',
+	},
+	formButton: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginHorizontal: '44%',
+		marginTop: '50%',
+	},
+});
