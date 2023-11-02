caf5c1732b0f7a5daaff5e4c6dfbcff01fd87213
Kirill Yaymchuk <stbestich@gmail.com>
2023-11-02 16:12:29 +0200

    Created list page

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './components/Login';
+import List from './components/List';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,7 +9,8 @@ const Navigate = () => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName='Login'>
-				<Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+				<Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
+				<Stack.Screen name='List' component={List} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
