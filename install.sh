0c0fe6559fb32ec1be0ba656a3c27d685e00abff
Kiril Yakymchuk <stbestich@gmail.com>
2023-12-17 11:52:26 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-# install dependencies
-npx expo install firebase
-npx expo customize metro.config.js
-npm install @react-navigation/native
-npx expo install react-native-screens react-native-safe-area-context
-npm i @gorhom/bottom-sheet
-expo install react-native-reanimated react-native-gesture-handler
-npm install @react-native-async-storage/async-storage
